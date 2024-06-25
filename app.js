8c24065eb8dae93ef88c49468406212ad08581d6
Harin-Mabao <mixharinmabao@gmail.com>
2024-06-26 03:14:24 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+let a = undefined;
+let b = null;
+let c = b + '4 2';
+
+let d = Number(a);
+let e = Number(b);
+let f = Number(c);
+
+console.log(d)
+console.log(e)
+console.log(f)
